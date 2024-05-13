199a1c77d2863c5129e59ad7f4841486add837cd
Sam Shahriari <samsha@kth.se>
2024-05-13 10:23:16 +0000

    add LSTM network

--- src/Networks.py
+++ src/Networks.py
@@ -13,3 +13,17 @@ class RNN(nn.Module):
         x = self.embed(x)
         output, last_hidden_state = self.RNN(x, h0)
         return self.final(all_hidden_states), last_hidden_state
+
+
+class LSTM(nn.Module):
+    def __init__(self, num_uniqeue_charachters, num_hidden_nodes, ouput_size, num_layers):
+        super(LSTM, self).__init__()
+
+        self.embed  = nn.Embedding(num_unique_chars, hidden_size, output_size)
+        self.LSTM = nn.LSTM(hidden_size, hidden_size, num_layers, batch_first=True)
+        self.final = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, h0):
+        x = self.embed(x)
+        output, last_hidden_state = self.LSTM(x, h0)
+        return self.final(all_hidden_states), last_hidden_state
