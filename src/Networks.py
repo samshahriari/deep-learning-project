0e1e22a47402b743364536c3babedcecef6cd2cf
Sam Shahriari <samsha@kth.se>
2024-05-13 10:20:26 +0000

    add RNN network

---
+++ src/Networks.py
@@ -0,0 +1,15 @@
+import torch
+from torch import nn
+
+class RNN(nn.Module):
+    def __init__(self, num_uniqeue_charachters, num_hidden_nodes, ouput_size):
+        super(RNN, self).__init__()
+
+        self.embed  = nn.Embedding(num_unique_chars, hidden_size, output_size)
+        self.RNN = nn.RNN(hidden_size, hidden_size, batch_first=True)
+        self.final = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, h0):
+        x = self.embed(x)
+        output, last_hidden_state = self.RNN(x, h0)
+        return self.final(all_hidden_states), last_hidden_state
