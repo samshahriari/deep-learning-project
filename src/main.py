1d164856cbb9d2332cf250985b5508f1e08970c5
Majd Dawli <dawli@kht.se>
2024-05-20 11:28:38 +0200

    Added grid search

--- src/main.py
+++ src/main.py
@@ -1,21 +1,33 @@
 from datasets import CharDataset, WordDataset
 from Networks import RNN, LSTM
 from Training import Training
-from nltk.tokenize import word_tokenize
+from itertools import product
 import bleu_score
 
-
 if __name__ == "__main__":
-    #todo context window behövs även i training
-    training_dataset = CharDataset('goblet_book.txt', 5, True  )#'/datasets/dd2417/glove.6B.50d.txt')
-    model = LSTM(len(training_dataset.id2token), 64, len(training_dataset.id2token), num_layers=2)#, embedding_size=training_dataset.embedding_dimension, embedding_weights=training_dataset.embeddings)
-    training_process = Training(model, 0.001, False, 25, training_dataset)
-    training_process.train()
+    param_grid = {
+        'node_number': [32, 64, 128, 256, 512, 1024],
+        'sequence_length': [5, 15, 30, 100]
+    }
 
-    generated_text = training_process.synthesize_text_char_model()
+    best_score = float('-inf')
+    best_params = None
 
-    print("BLEU score:")
-    print(bleu_score.calc_BLEU(generated_text, 'goblet_book.txt'))
+    for params in product(param_grid['node_number'], param_grid['sequence_length']):
+        print("Training with parameters:", params)
+        training_dataset = CharDataset('goblet_book.txt', params[1], True)
+        model = LSTM(len(training_dataset.id2token), params[0], len(training_dataset.id2token), num_layers=2)
+        training_process = Training(model, 0.001, False, 25, training_dataset)
+        training_process.train()
 
+        generated_text = training_process.synthesize_text_char_model()
 
+        bleu = bleu_score.calc_BLEU(generated_text, 'goblet_book.txt')
+        print(f"Parameters: {params}, BLEU score: {bleu}")
 
+        if bleu > best_score:
+            best_score = bleu
+            best_params = params
+
+    print("Best parameters:", best_params)
+    print("Best BLEU score:", best_score)
